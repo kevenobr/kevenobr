021e7ee030258352a95262369a08f4e9d967eb1c
kevenobr <kevenobr@gmail.com>
2024-10-21 19:41:32 -0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function greetUser() {
- const nameInput = document.getElementById("nameInput");
- const name = nameInput.value;
-
- const greetingElement = document.getElementById("helloArea");
- greetingElement.textContent = "hello, " + name + "!";
-}

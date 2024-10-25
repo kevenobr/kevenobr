3ea4915368192f02f72031085e3afdaf8d8fe5aa
kevenobr <kevenobr@gmail.com>
2024-10-25 14:23:27 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+function greetUser() {
+ const nameInput = document
+  .getElementById("nameInput");
+ const name = nameInput.value;
+
+ const greetingElement = document
+  .getElementById("helloArea");
+ greetingElement.textContent =
+  "hello, " + name + "!";
+}
+
+//hamburguer menu //
+
+const hamMenu = document
+ .querySelector(".ham-menu");
+
+const offScreenMenu = document
+ .querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click",
+ () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle(
+   "active");
+ });
